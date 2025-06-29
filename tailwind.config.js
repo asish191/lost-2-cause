338c1abdeae49212c790f664cdff13462b550018
UmairSaeedBhatti <us5014302@gmail.com>
2025-06-29 11:53:18 +0200

    tailwind configuration added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6B46C1',
+        secondary: '#9B59B6',
+        accent: '#D6BCFA',
+        background: '#F5F6FA',
+        text: '#4A4A4A',
+        highlight: '#FFFFFF',
+        success: '#48BB78',
+        gradientStart: '#C3B1E1',
+        gradientEnd: '#6B46C1',
+      },
+    },
+  },
+  plugins: [],
+};
